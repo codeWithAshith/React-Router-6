5b4ba5d96399b19c24e91fd79946ab84f9d1a371
codewithashith <codewithashith@gmail.com>
2023-02-09 10:17:44 +0530

    13. Updating Single Product Page

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 
+import products from "../data";
+
 const SingleProduct = () => {
   const { productId } = useParams();
-  
+
+  const product = products.find((product) => product.id === productId);
+
   return (
     <section className="section product">
-      <h2>{productId}</h2>
+      <img src={product.image} alt={product.name} />
+      <h2>{product.name}</h2>
       <Link to="/" className="btn">
         Home
       </Link>
