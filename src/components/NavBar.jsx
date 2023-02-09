b4360f130650972c51dbf3590ab1f2983acfac0e
codewithashith <codewithashith@gmail.com>
2023-02-09 10:04:55 +0530

    09. NavLink - active page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <>
       <nav className="navbar">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/products">Products</Link>
+        <NavLink
+          to="/"
+          //   style={({ isActive }) => {
+          // return { color: isActive ? "red" : "grey" };
+          //   }}
+          className={({ isActive }) => (isActive ? "link active" : "link")}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "link active" : "link")}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/products"
+          className={({ isActive }) => (isActive ? "link active" : "link")}
+        >
+          Products
+        </NavLink>
       </nav>
     </>
   );
