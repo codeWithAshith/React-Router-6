ee905a33b6df221d23447a3a7f76b390d7b6d3e0
codewithashith <codewithashith@gmail.com>
2023-02-09 09:21:06 +0530

    02. First pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 // npm install react-router-dom
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 const App = () => {
-  return <h2>React Router 6 Tutorial</h2>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Homepage</div>} />
+        <Route
+          path="/testing"
+          element={
+            <div>
+              <h2>Testing</h2>
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
