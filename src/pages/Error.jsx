a160d8b39e1b3d1d183dc33331326c0c15c3c131
codewithashith <codewithashith@gmail.com>
2023-02-09 10:10:16 +0530

    11. UI fix

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <>
+    <section className="section">
       <h2>404 Error</h2>
       <p>Page Not Found</p>
       <Link to="/" className="btn">
         Return Home
       </Link>
-    </>
+    </section>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <>
+    <section className="section">
       <h2>Home</h2>
       <Link to="/about" className="btn">
         About
       </Link>
-    </>
+    </section>
   );
 };
 
