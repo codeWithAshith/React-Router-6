228eaa9deb51137b510a91a763b6e6cfcae6b375
codewithashith <codewithashith@gmail.com>
2023-02-09 09:49:53 +0530

    08. Adding Navbar to Shared Layout

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/products">Products</Link>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
--- src/components/SharedLayout.jsx
+++ src/components/SharedLayout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import NavBar from "./NavBar";
 
 const SharedLayout = () => {
   return (
     <>
-      <h2>Shared Layout</h2>
+      <NavBar />
       <Outlet />
     </>
   );
